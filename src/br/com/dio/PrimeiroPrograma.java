e518b1373aafc1856bf25a461005d48b0331eeb4
ardassejose <josevictorps3lima@gmail.com>
2023-02-22 19:03:03 -0300

    adicionado classe Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,9 +1,13 @@
 package br.com.dio;
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
       Gato gato = new Gato();
       System.out.println(gato);
+
+      Livro livro1 = new Livro("O problema das estrelas", 300);
+      System.out.println(livro1);
     }
 }
